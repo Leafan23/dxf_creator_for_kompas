9fa618df163e0300fab6d83de1876c09e9d893b4
Leafan23 <la2player_07@mail.ru>
2026-07-20 09:54:08 +0300

    v0.1 (Build 3)

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from time import sleep
 
 import pythoncom
 from win32com.client import Dispatch, gencache, VARIANT
@@ -8,23 +9,28 @@ import sys
 
 
 class KompasAPI:
-    def __init__(self):
+    def __init__(self, kompas_document=None):
         self.ShowOnSheet = 0
         #  Подключим описание интерфейсов API7
         self.api7 = gencache.EnsureModule("{69AC2981-37C0-4379-84FD-5DD2F3C0A520}", 0, 1, 0)
         self.application = self.api7.IApplication(
             Dispatch("Kompas.Application.7")._oleobj_.QueryInterface(self.api7.IApplication.CLSID,
                                                                      pythoncom.IID_IDispatch))
-        self.kompas_document = self.application.ActiveDocument
-        self.kompas_document_3d_1 = self.api7.IKompasDocument3D1(self.kompas_document)
-        self.kompas_document_3d = self.api7.IKompasDocument3D(self.kompas_document)
-        self.view_projection_manager = self.kompas_document_3d_1.ViewProjectionManager
-        self.selection_manager = self.kompas_document_3d.SelectionManager
-        print(self.selection_manager.SelectedObjects)
+        if not None:
+            self.kompas_document = self.application.ActiveDocument
+        else:
+            self.kompas_document = kompas_document
+        self.documents = self.application.Documents
+        if self.kompas_document.DocumentType == 4:
+            self.kompas_document_3d_1 = self.api7.IKompasDocument3D1(self.kompas_document)
+            self.kompas_document_3d = self.api7.IKompasDocument3D(self.kompas_document)
+            self.view_projection_manager = self.kompas_document_3d_1.ViewProjectionManager
+            self.selection_manager = self.kompas_document_3d.SelectionManager
+            print(self.selection_manager.SelectedObjects)
 
 
     def add_view(self, view_name):
-        "добавить вид с именем"
+        """добавить вид с именем"""
         view_projection_7 = self.view_projection_manager.Add()
         view_projection_7.Name = view_name
         view_projection_7.Update()
@@ -39,6 +45,7 @@ if __name__ == '__main__':
 
     api = KompasAPI()
     print(api.view_projection_manager.OrientationNormalTo(api.selection_manager.SelectedObjects))
+    sleep(1)
     api.view_projection_manager.OrientationNormalTo(api.selection_manager.SelectedObjects)
     api.add_view('kkk1')
     print(api.view_projection_manager.BaseUserOrientation)
@@ -57,5 +64,7 @@ if __name__ == '__main__':
         print('-------------------------------')
         ViewProjectionManager.Update()
 
-
+    # Создать пустой чертеж, без рамки
+    api_drawing = KompasAPI(api.documents.Add(1,True))
+    print(api_drawing.kompas_document.DocumentType)
 
