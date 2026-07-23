60a1a16e9d51adc20cee4bd898e8d0c30ffcfa7f
Leafan23 <la2player_07@mail.ru>
2026-07-23 17:08:32 +0300

    v0.3 (Build 30)
    - минорные изменения

--- GUI.py
+++ GUI.py
@@ -6,18 +6,14 @@ import pywinstyles
 class App(Tk.CTk):
     def __init__(self):
         super().__init__()
-        pywinstyles.apply_style(self, 'acrylic')
         self.files_for_dxf_creation = []
 
-        self.drop_zone = Tk.CTkFrame(master=self, width=300, height=300)
-        pywinstyles.apply_dnd(self.drop_zone, self.drop_func)
+        self.drop_zone = DndZone(master=self, width=300, height=300, corner_radius=15)
         self.drop_zone.pack()
         button_1 = Tk.CTkButton(self, text='Создать dxf', command=self.button_create_dxf)
         button_1.pack()
         button_2 = Tk.CTkButton(self, text='Создать группу dxf', command=self.create_dxf_pack)
         button_2.pack()
-
-
 
     def button_create_dxf(self):
         API.CreateDxf()
@@ -28,8 +24,16 @@ class App(Tk.CTk):
                 if Path(i).exists():
                     API.CreateDxf(i)
 
-    def drop_func(self, pathfile):
-        self.files_for_dxf_creation.extend(pathfile)
-        self.files_for_dxf_creation = list(dict.fromkeys(self.files_for_dxf_creation))
-        print(len(self.files_for_dxf_creation))
 
+class DndZone(Tk.CTkFrame):
+    def __init__(self, master, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        pywinstyles.apply_dnd(self, self.drop_func)
+
+        label_1 = Tk.CTkLabel(master=self, text='Перетащите сюда файлы', width=300, height=300,)
+        label_1.pack()
+
+    def drop_func(self, pathfile):
+        self.master.files_for_dxf_creation.extend(pathfile)
+        self.master.files_for_dxf_creation = list(dict.fromkeys(self.master.files_for_dxf_creation))
+        print(len(self.master.files_for_dxf_creation))
