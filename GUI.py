82abde2dc52712482a5a86e1fde4567dd21ba075
Leafan23 <la2player_07@mail.ru>
2026-07-24 15:11:16 +0300

    v0.4 (Build 33)
    - добавлен файл для замены py_win_style.py

--- GUI.py
+++ GUI.py
@@ -3,37 +3,41 @@ import API
 from pathlib import Path
 import pywinstyles
 
+
+
 class App(Tk.CTk):
     def __init__(self):
         super().__init__()
         self.title("dxf creator")
         self.attributes("-topmost", True)
+        self.main_fraim = MainFrame(master=self)
+        self.main_fraim.pack()
+
+
+
+class MainFrame(Tk.CTkFrame):
+    def __init__(self, master, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
         self.files_for_dxf_creation = []
-        #pywinstyles.apply_style(self, "transparent")
-        #pywinstyles.change_header_color(self, color="blue")
 
         self.drop_zone = DndZone(master=self, width=300, height=300, corner_radius=15)
         self.drop_zone.grid(row=0, column=0, columnspan=2)
-        #self.drop_zone.bind('<Enter>', self.on_start_hover)
-        #self.drop_zone.bind('<Leave>', self.on_end_hover)
 
-        button_1 = Tk.CTkButton(self, text='Создать dxf', command=self.button_create_dxf)
-        button_1.grid(row=1, column=0)
+        button_1 = Tk.CTkButton(self, text='Создать dxf', command=API.CreateDxf)
+        button_1.grid(row=1, column=0, sticky='ew', padx=3, pady=3)
         self.button_2 = Tk.CTkButton(self,
                                      text='Создать группу dxf',
                                      command=self.create_dxf_pack,
                                      state="disabled",
                                      fg_color='orange',
                                      text_color='black')
-        self.button_2.grid(row=1, column=1)
+        self.button_2.grid(row=1, column=1, sticky='ew', padx=3, pady=3)
 
-
-
-    def button_create_dxf(self):
-        API.CreateDxf()
+        self.drawing_zone = DrawingZone(master=self)
+        self.drawing_zone.grid(row=2, column=0, columnspan=2, sticky='ew')
 
     def create_dxf_pack(self):
-        if len(self.files_for_dxf_creation) > 0: # проверка на пустой список
+        if len(self.files_for_dxf_creation) > 0:  # проверка на пустой список
             for i in self.files_for_dxf_creation:
                 if Path(i).exists():
                     API.CreateDxf(i)
@@ -55,8 +59,9 @@ class DndZone(Tk.CTkFrame):
         super().__init__(master, *args, **kwargs)
         pywinstyles.apply_dnd(self, self.drop_func)
 
-        label_1 = Tk.CTkLabel(master=self, width=300, height=300,)
-        label_1.configure(text='Перетащите сюда файлы')
+        label_1 = Tk.CTkLabel(master=self, width=300, height=300)
+        label_1.configure(
+            text='Перетащите сюда файлы деталей компас-3d (.m3d) \n и нажмите "Создать группу dxf"')
         label_1.pack()
 
     def drop_func(self, pathfile):
@@ -64,3 +69,32 @@ class DndZone(Tk.CTkFrame):
         self.master.files_for_dxf_creation = list(dict.fromkeys(self.master.files_for_dxf_creation))
         print(len(self.master.files_for_dxf_creation))
         self.master.button_2.configure(state="normal")
+        self.master.drawing_zone.create_string()
+
+
+
+class DrawingZone(Tk.CTkScrollableFrame):
+    def __init__(self, master, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        self.drawing_strings = []
+        self.master = master
+        for i in master.files_for_dxf_creation:
+            self.drawing_strings.append(DrawingString(master=self).pack())
+
+    def create_string(self):
+        print('create_string')
+        print(self.master.files_for_dxf_creation)
+
+
+
+
+class DrawingString(Tk.CTkFrame):
+    def __init__(self, master, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+
+        self.label = Tk.CTkLabel(self)
+        self.label.pack()
+        #Строка пути
+        #Кнопка удаления
+
+
